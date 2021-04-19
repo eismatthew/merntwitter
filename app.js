2495a7c867b633eda8c1fbe94fb3191845e70876
Matthew Eis <eismatthewd@gmail.com>
2021-04-18 21:34:59 -0700

    final commit

--- app.js
+++ app.js
@@ -14,7 +14,14 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+  const path = require('path');
 
+  if (process.env.NODE_ENV === 'production') {
+      app.use(express.static('frontend/build'));
+      app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+      })
+  }
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
