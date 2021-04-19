827b51aca8339d890f049ea660e1181ac7bcfc92
Matthew Eis <eismatthewd@gmail.com>
2021-04-18 21:24:47 -0700

    final commit

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users")
+const tweets = require("./routes/api/tweets")
+const User = require('./models/User');
+const passport = require('passport');
+
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -10,9 +15,30 @@ mongoose
   .catch(err => console.log(err));
 
 
-const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Hello World 2"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(passport.initialize());
+
+require('./config/passport')(passport);
+
+// app.get("/", (req, res) => {
+//     const user = new User({
+//         handle: 'matt',
+//         email: 'matt@matt.matt',
+//         password: 'mattisgreat123'
+//     })
+//     user.save()
+//     res.send("Hello World");
+
+// });
+
+
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
